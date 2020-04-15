6868b7083d034199d036aa55d0d06bd90a96dec9
Rohit Ashiwal <rohit.ashiwal265@gmail.com>
2020-04-15 13:07:09 +0530

    initial commit

---
+++ script/data.js
@@ -0,0 +1,13 @@
+/**
+ * This file contains the information of participants which will be
+ * displayed using the magical powers of JavaScript.
+ *
+ * format:
+ * 	{
+ * 		"name": "Your Name",
+ * 		"desc": "What do you do for fun?",
+ * 		"github": "You GitHub Handle URL!"
+ * 	}
+ */
+const data = `[
+]`;
